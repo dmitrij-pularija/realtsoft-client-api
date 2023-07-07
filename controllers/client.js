222d00c7f012e00a150562d669eabe22a9dfef15
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-07-07 11:20:43 +0300

    code refactoring

--- controllers/client.js
+++ controllers/client.js
@@ -2,6 +2,6 @@ const { addClient, getClient } = require("../models/client/operations");
 
 const add = async ({ body: { phones, name, email, userId } }) =>
   await addClient(phones, name, email, userId);
-const get = async ({ body: { phone, email } }) => await getClient(phone, email);
+const get = async ({ query: { phone, email } }) => await getClient(phone, email);
 
 module.exports = { add, get };
